0da8a0952d7945738457dbf3c84809c102163c97
Akeem Roy Allen <allenakeem8@gmail.com>
2020-01-23 15:03:33 -0500

    Mongoose now connects to localmongo server

--- backend/server.js
+++ backend/server.js
@@ -7,15 +7,19 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-// const uri = process.env.LOCAL_URI;
+const uri = process.env.LOCAL_URI;
 
-// mongoose.connect(uri, { useCreateIndex: true, useNewUrlParser: true });
+mongoose.connect(uri, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
-// const connection = mongoose.connection;
+const connection = mongoose.connection;
 
-// connection.once("open", () => {
-//   console.log("MongoDb Connection established successfully");
-// });
+connection.once("open", () => {
+  console.log("MongoDb Connection established successfully");
+});
 
 app.use(cors);
 app.use(express.json());
