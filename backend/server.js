c0ef1a9f90c7285dc3554384b18e6378ec207bd7
Akeem Roy Allen <allenakeem8@gmail.com>
2020-01-23 10:15:45 -0500

    Added Frontend with react and backend with express and mongoose

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// const uri = process.env.LOCAL_URI;
+
+// mongoose.connect(uri, { useCreateIndex: true, useNewUrlParser: true });
+
+// const connection = mongoose.connection;
+
+// connection.once("open", () => {
+//   console.log("MongoDb Connection established successfully");
+// });
+
+app.use(cors);
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
