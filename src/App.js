8f002391e27c65a3bbc5723d57269c963abd3565
RXM1348 <roxannemeeks2@gmail.com>
2020-02-11 18:54:59 -0500

    Created Login page

--- src/App.js
+++ src/App.js
@@ -2,27 +2,12 @@ import React from 'react';
 import greatnessGif from './assets/images/greatness.gif';
 import Button from '@material-ui/core/Button';
 import './App.css';
+import Login from './pages/login';
 
 function App() {
   return (
     <div className="App">
-      <p>
-        <h1>WELCOME...</h1> <h2>Roxy and Rico</h2> to the start of great
-        things!!
-      </p>
-      <p>
-        This is the canvas upon which we shall build a system like{' '}
-        <strong>NO OTHER</strong>!!!
-      </p>
-      <img src={greatnessGif} alt="We are the champions" />
-      <p>Prepare to become greatness!!!</p>
-      <Button
-        variant="outlined"
-        color="primary"
-        onClick={() => alert('Button Clicked')}
-      >
-        This Button is from the Material Ui Library
-      </Button>
+      <Login />
     </div>
   );
 }
---
+++ src/pages/login.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Input from '@material-ui/core/Input';
+import { Divider, Button } from '@material-ui/core';
+import axios from 'axios';
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        Username: '',
+        Password: ''
+    };
+  }
+  handleUsername = (event) => {
+    this.setState({Username: event.target.value});
+  }
+  handlePassword = (event) => {
+    this.setState({Password: event.target.value});
+  }
+  handleSubmit = (event) => {
+    event.preventDefault();
+    axios.post('https://mysterious-caverns-49185.herokuapp.com/users/login', {
+        userName: this.state.Username,
+        password: this.state.Password
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h1>Login</h1>
+        <Input placeholder="Username" value= {this.state.Username} onChange={this.handleUsername} />
+        <br />
+        <Input placeholder="Password" value= {this.state.Password} type="password" onChange={this.handlePassword}/>
+        <br />
+        <Button type="submit">Submit</Button>
+      </form>
+    );
+  }
+}
+export default Login;
