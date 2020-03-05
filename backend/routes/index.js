079533b8581f444c853d3a6741ef1f2cb38eb566
Akeem Allen <allenakeem8@protonmail.com>
2020-03-05 18:28:17 -0500

    Moved middleware to middleware file

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const usersRouter = require('./user');
 const productsRouter = require('./product');
+const imagesRouter = require('./images');
 
 router.use('/users', usersRouter);
 router.use('/products', productsRouter);
+router.use('/images', imagesRouter);
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,91 +1,23 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const morgan = require('morgan');
-const swaggerUi = require('swagger-ui-express');
-const swaggerJsdoc = require('swagger-jsdoc');
 const routes = require('./routes');
-const fs = require('fs');
-const multer = require('multer');
-const path = require('path');
-const crypto = require('crypto');
-const GridFsStorage = require('multer-gridfs-storage');
-const Grid = require('gridfs-stream');
-const methodOverride = require('method-override');
-
-const swaggerOptions = {
-  swaggerDefinition: {
-    info: {
-      title: 'Cups Api',
-      description:
-        'An API for the new cups system to be implemented in coffee shops wordwide',
-      contact: {
-        name: 'Akeem Allen, Richard Robinson'
-      },
-      servers: [
-        'https://mysterious-caverns-49185.herokuapp.com/',
-        'http://localhost:5000/'
-      ]
-    }
-  },
-  apis: ['./routes/*.js']
-};
-
-const swaggerDocs = swaggerJsdoc(swaggerOptions);
+const middleWare = require('./middleware');
 
 require('dotenv').config();
 
 const app = express();
-
 const uri = process.env.ATLAS_URI || 'mongodb://localhost:27017/cupsdatabase';
-mongoose.connect(uri, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-  useCreateIndex: true
-});
-
-let gfs;
-let connection = mongoose.connection;
-
-connection.once('open', () => {
-  // eslint-disable-next-line no-console
-  // Initialize Stream
-  gfs = Grid(connection.db, mongoose.mongo);
-  gfs.collection('uploads');
-  console.log('MongoDB connection established successfully');
-});
-
-// Create Storage Engine
-const storage = new GridFsStorage({
-  url: uri,
-  options: { useNewUrlParser: true, useUnifiedTopology: true },
-  file: (req, file) => {
-    return new Promise((resolve, reject) => {
-      crypto.randomBytes(16, (err, buf) => {
-        if (err) {
-          return reject(err);
-        }
-        const filename = buf.toString('hex') + path.extname(file.originalname);
-        const fileInfo = {
-          filename: filename,
-          bucketName: 'uploads'
-        };
-        resolve(fileInfo);
-      });
-    });
-  }
-});
-const upload = multer({ storage });
 
 // MiddleWare
-app.use(cors());
 app.use(express.json());
-app.use(morgan('combined'));
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-app.use('/', routes);
-app.use(methodOverride('_method'));
+middleWare.mongoConnection(uri);
+middleWare.useCors(app);
+middleWare.useMorgan(app);
+middleWare.useSwagger(app);
+middleWare.useMethodOverride(app);
+const upload = middleWare.useFileUpload(uri);
 
-// Upload Files
+app.use('/', routes);
+
 app.post('/upload', upload.single('file'), (req, res) => {
   res.json({ file: req.file });
 });
