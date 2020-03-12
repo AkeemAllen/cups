2086842b2f7809126a2930389aea0c0e8566d552
RXM1348 <roxannemeeks2@gmail.com>
2020-03-12 11:28:11 -0500

    added order routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const usersRouter = require('./user');
 const productsRouter = require('./product');
+const ordersRouter = require('./order');
 
 router.use('/users', usersRouter);
+router.use('/orders', ordersRouter);
 router.use('/products', productsRouter);
 
 module.exports = router;
