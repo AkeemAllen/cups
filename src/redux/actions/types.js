d0da7b9aea4290a9aa90a0c9c7a6b607d4db31d1
RXM1348 <roxannemeeks2@gmail.com>
2020-03-19 21:31:31 -0500

    Moved delete function to redux store

--- src/redux/actions/types.js
+++ src/redux/actions/types.js
@@ -2,4 +2,5 @@ export const AUTH_USER = 'AUTH_USER';
 export const GET_USER = 'GET_USER';
 
 export const FETCH_PRODUCTS = 'FETCH_PRODUCTS';
+export const DELETE_PRODUCT = 'DELETE_PRODUCT';
 export const NEW_PRODUCT = 'NEW_PRODUCT';
