0654c33a5d0f36730f4572f46b7a16a89722079e
Akeem Roy Allen <allenakeem8@gmail.com>
2020-04-22 09:38:46 -0500

    Message After Order Completion

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { IconButton, Drawer, List, ListItem, Button } from '@material-ui/core';
+import {
+  IconButton,
+  Drawer,
+  List,
+  ListItem,
+  Button,
+  Modal,
+  Backdrop
+} from '@material-ui/core';
 import { ShoppingCart, Delete, AttachMoney } from '@material-ui/icons';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
@@ -8,6 +16,7 @@ import { bindActionCreators } from 'redux';
 
 function Cart(props) {
   const [open, setOpen] = React.useState(false);
+  const [openModal, setModal] = React.useState(false);
 
   const handleOpen = () => {
     setOpen(true);
@@ -15,6 +24,10 @@ function Cart(props) {
 
   const handleClose = () => {
     setOpen(false);
+  };
+
+  const handleCloseModal = () => {
+    setModal(false);
   };
 
   const itemNumber = props.cart.length;
@@ -86,12 +99,28 @@ function Cart(props) {
             color: 'white'
           }}
           disabled={props.cart.length <= 0}
-          onClick={() =>
-            props.placeOrder(props.user, props.cart, props.totalCost)
-          }
+          onClick={() => {
+            props.placeOrder(props.user, props.cart, props.totalCost);
+            setModal(true);
+          }}
         >
           Place Order
         </Button>
+        <Modal
+          style={styles.modal}
+          open={openModal}
+          onClose={handleCloseModal}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{
+            timeout: 500
+          }}
+        >
+          <div style={styles.modalMessage}>
+            <h1>Order Successful</h1>
+            <h3>Thanks for coming to cups</h3>
+          </div>
+        </Modal>
       </Drawer>
     </div>
   );
@@ -117,3 +146,21 @@ const mapDispatchToProps = dispatch => ({
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Cart);
+
+const styles = {
+  modalMessage: {
+    display: 'flex',
+    flexDirection: 'column',
+    padding: '30px',
+    backgroundColor: 'white',
+    borderRadius: '10px',
+    boxShadow: '0px 0px 9px 0px rgba(0,0,0,0.7)'
+  },
+  modal: {
+    justifyContent: 'center',
+    display: 'flex',
+    width: '100%',
+    alignItems: 'center',
+    height: '100vh'
+  }
+};
