41565f7d5190e85c84c41995914b3e7b5d8c2412
Akeem Roy Allen <allenakeem8@gmail.com>
2020-04-26 10:24:06 -0500

    Adding build folder

---
+++ build/precache-manifest.43e41c2d411ed65bfb479e4cd80ebdc0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c841b395017426961764813ae33352c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1249c71971c28d911a16",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2e66725735b0b03afa21",
+    "url": "/static/js/2.58b35913.chunk.js"
+  },
+  {
+    "revision": "cc08cf93cc1fae2b308c598502122ede",
+    "url": "/static/js/2.58b35913.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1249c71971c28d911a16",
+    "url": "/static/js/main.0cb9bf21.chunk.js"
+  },
+  {
+    "revision": "dae0e55f20986d43f8ea",
+    "url": "/static/js/runtime-main.4672174d.js"
+  },
+  {
+    "revision": "509b3635cae35c0ee049d92ab3c2d512",
+    "url": "/static/media/coffee-background_3.509b3635.jpg"
+  },
+  {
+    "revision": "368d4cdd0abe0ee8dd4346327031ab94",
+    "url": "/static/media/coffee-logo2.368d4cdd.png"
+  },
+  {
+    "revision": "7c754a307141825038b0cd8805e7d54c",
+    "url": "/static/media/coffee.7c754a30.png"
+  },
+  {
+    "revision": "b538f3cde7cdf70828be4710f4ebe149",
+    "url": "/static/media/coffeeLogo1.b538f3cd.png"
+  },
+  {
+    "revision": "2b970e1eaa0ac941bf30476adec847d4",
+    "url": "/static/media/sugarbun.2b970e1e.jpg"
+  }
+]);
